d7aae163482b5af5d0ec1c4b957cb851f76e615d
zoe1991 <275738712@qq.com>
2016-12-12 08:35:05 +0800

    上传

---
+++ ZoeKVO/NSObject+Zoe_KVO.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+Zoe_KVO.h
+//  ZoeKVO
+//
+//  Created by mac on 2016/12/11.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject_Zoe_KVO : NSObject
+
+@end
---
+++ ZoeKVO/NSObject+Zoe_KVO.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+Zoe_KVO.m
+//  ZoeKVO
+//
+//  Created by mac on 2016/12/11.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import "NSObject+Zoe_KVO.h"
+
+@implementation NSObject_Zoe_KVO
+
+@end
